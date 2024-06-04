00ca3b459b3929aedfadfe7a40a4cbf94f591be7
YesI4m <111292812+YesI4m@users.noreply.github.com>
2024-06-04 16:09:43 +0900

    aa (#21)
    * j
    
    * W - (You and Me! / Change of Plan)
    
    * movie 페이지 css
    
    * fd
    
    * n
    
    * fssd
    
    * aaaa
    
    ---------
    
    Co-authored-by: YongQ <112691069+yongqyu49@users.noreply.github.com>

---
+++ Yeongkkeul/src/main/java/service/movie/ManagerMovieService.java
@@ -0,0 +1,29 @@
+package service.movie;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import control.CommandProcess;
+import dao.MovieDAO;
+import dto.LikeMovie;
+
+public class ManagerMovieService implements CommandProcess{
+
+	@Override
+	public String requestProc(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {System.out.println("addMovie");
+			System.out.println("selmove");
+			MovieDAO md = MovieDAO.getInstance();
+			List<LikeMovie> ld = md.selectMovie();
+			System.out.println(ld);
+			request.setAttribute("ld", ld);
+
+		return "/view/movie/admin.jsp";
+		}
+	}
+
+
